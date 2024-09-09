e83a06802bf3eb8c590e5631edbf71b8ba4ca1ff
MarceloMindas <160534419+MarceloMindas@users.noreply.github.com>
2024-09-09 09:11:33 -0300

    Lógica Python Faesa

---
+++ Exercícios/Exercicio.py
@@ -0,0 +1,7 @@
+m1 = float(input("Digite o valor1: "))
+m2 = float(input("Digite o valor2: "))
+m3 = float(input("Digite o valor3: "))
+
+media = (m1 + m2 + m3) /  3
+
+print ("a sua média é : ", + media)
